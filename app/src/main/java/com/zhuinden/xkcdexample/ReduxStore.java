9215cf9ee225fc2786f86f1a287ae66e67cad013
zhuinden <zhuinden@gmail.com>
2017-04-13 14:29:41 +0200

    the store might actually want to store something.

--- app/src/main/java/com/zhuinden/xkcdexample/ReduxStore.java
+++ app/src/main/java/com/zhuinden/xkcdexample/ReduxStore.java
@@ -11,17 +11,21 @@ import io.reactivex.Single;
  */
 
 public class ReduxStore {
-    BehaviorRelay<State> state = BehaviorRelay.createDefault(State.create(new StateBundle(), Action.INIT));
+    private State previousState = State.create(new StateBundle(), Action.INIT);
+
+    BehaviorRelay<State> state = BehaviorRelay.createDefault(previousState);
 
     RootReducer reducer = RootReducer.builder().addReducer(new XkcdReducer()).build();
 
-    public Observable<State> state() {
-        return state;
+    public Observable<StateChange> state() {
+        return state.map(currentState -> StateChange.create(previousState, currentState));
     }
 
     public Single<StateChange> dispatch(Action action) {
-        final State previousState = state.getValue();
+        final State currentState = state.getValue();
         Single<State> _newState = reducer.reduce(state.getValue(), action);
-        return _newState.map(newState -> StateChange.create(previousState, newState));
+        return _newState.map(newState -> StateChange.create(currentState, newState))
+                .doOnSuccess(stateChange -> previousState = stateChange.previousState())
+                .doOnSuccess(stateChange -> state.accept(stateChange.newState()));
     }
 }
