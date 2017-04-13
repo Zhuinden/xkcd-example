8bd23ad4efdf2455b9eee98a7e771ee41e81a341
zhuinden <zhuinden@gmail.com>
2017-04-13 16:48:06 +0200

    scan totally doesn't work. ¬¬

--- app/src/main/java/com/zhuinden/xkcdexample/redux/ReduxStore.java
+++ app/src/main/java/com/zhuinden/xkcdexample/redux/ReduxStore.java
@@ -17,9 +17,26 @@ public class ReduxStore {
 
     private final State initialState = State.create(new StateBundle(), Action.INIT);
 
+    private State previousState = initialState;
+
     private RootReducer reducer;
 
     BehaviorRelay<State> state = BehaviorRelay.createDefault(initialState);
+
+    public State getState() {
+        return state.getValue();
+    }
+
+    public boolean isAtInitialState() {
+        return this.state.getValue() == initialState;
+    }
+
+    public void setInitialState(State state) {
+        if(this.state.getValue() != initialState) {
+            throw new IllegalStateException("Initial state cannot be set after internal state has already been modified!");
+        }
+        this.state.accept(state);
+    }
 
     public static Builder builder() {
         return new Builder();
@@ -42,14 +59,15 @@ public class ReduxStore {
     }
 
     public Observable<StateChange> state() {
-        return state.scan(StateChange.create(initialState, initialState),
-                (stateChange, newState) -> StateChange.create(stateChange.newState(), newState));
+        return state.map(currentState -> StateChange.create(previousState,
+                currentState)); // TODO: figure out how to make this work with Scan()
     }
 
     public Single<StateChange> dispatch(Action action) {
         final State currentState = state.getValue();
         Single<State> _newState = reducer.reduce(state.getValue(), action);
         return _newState.map(newState -> StateChange.create(currentState, newState))
+                .doOnSuccess(stateChange -> previousState = stateChange.previousState())
                 .doOnSuccess(stateChange -> state.accept(stateChange.newState()));
     }
 }
